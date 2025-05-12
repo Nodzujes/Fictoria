9a28541704ef529898164bff9a2d893fea61b55e
Kiquzo <pedrossereda@gmail.com>
2025-05-13 02:16:46 +0300

    [FIX] - стили для метрики

--- src/components/MetricHeader.jsx
+++ src/components/MetricHeader.jsx
@@ -6,7 +6,10 @@ const MetricHeader = () => {
         <header className="MetricHeader">
             <h1>Fictoria | Метрика</h1>
             <Link to="/admin" style={{ color: '#5AB9EA', textDecoration: 'none', marginLeft: '20px' }}>
-                Вернуться в админ-панель
+                Админ панель
+            </Link>
+            <Link to="/" style={{ color: '#5AB9EA', textDecoration: 'none', marginLeft: '20px' }}>
+                Главная страница
             </Link>
         </header>
     );
