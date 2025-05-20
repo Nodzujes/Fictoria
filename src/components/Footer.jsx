19dcf648d427db2ad2b45551a698e77e4ff7462b
Kiquzo <pedrossereda@gmail.com>
2025-05-20 04:07:41 +0300

    [ADD] - адаптив

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,30 @@
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
+import { useState, useRef } from 'react';
+import ModalUser from '../components/ModalUser.jsx';
+import { useUser } from '../context/UserContext.jsx';
 
 function Footer() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const userButtonRef = useRef(null);
+    const { user, logout, isLoading } = useUser();
+
+    const handleUserIconClick = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleLogout = async () => {
+        try {
+            await logout();
+            setIsModalOpen(false);
+        } catch (error) {
+            console.error('Ошибка при выходе:', error.message);
+        }
+    };
+
+    if (isLoading) {
+        return <div>Загрузка...</div>;
+    }
+
     return (
         <>
             <footer>
@@ -41,13 +65,78 @@ function Footer() {
                         <div className="footer__social--links">
                             <a href=""><img src="/icons/vk.png" alt="vk icon" />ВКонтакте</a>
                             <a href=""><img src="/icons/tg.png" alt="tg icon" />Телеграмм</a>
-                            <a href=""><img src="/icons/yt.png" alt="tg icon" />YouTube</a>
+                            <a href=""><img src="/icons/yt.png" alt="yt icon" />YouTube</a>
                         </div>
                     </div>
                 </div>
+                <div className="wrapper__mobile">
+                    <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : '')}>
+                        {({ isActive }) => (
+                            <img
+                                className="footer_home"
+                                src={isActive ? "/icons/home-active.png" : "/icons/home-page.png"}
+                                alt="иконка главной страницы"
+                            />
+                        )}
+                    </NavLink>
+                    <NavLink to="/search" className={({ isActive }) => (isActive ? 'active' : '')}>
+                        {({ isActive }) => (
+                            <img
+                                className="footer_search"
+                                src={isActive ? "/icons/search-active.png" : "/icons/search-line.png"}
+                                alt="иконка поиска"
+                            />
+                        )}
+                    </NavLink>
+                    <NavLink to="/creator" className={({ isActive }) => (isActive ? 'active' : '')}>
+                        {({ isActive }) => (
+                            <img
+                                className="footer_edit"
+                                src={isActive ? "/icons/edit-active.png" : "/icons/edit-line.png"}
+                                alt="иконка перехода на конструктор"
+                            />
+                        )}
+                    </NavLink>
+                    <NavLink to="/setings" className={({ isActive }) => (isActive ? 'active' : '')}>
+                        {({ isActive }) => (
+                            <img
+                                className="footer_setings"
+                                src={isActive ? "/icons/settings-active.png" : "/icons/settings-line.png"}
+                                alt="иконка настроек"
+                            />
+                        )}
+                    </NavLink>
+                    {user ? (
+                        <button
+                            className="header__user"
+                            ref={userButtonRef}
+                            onClick={handleUserIconClick}
+                        >
+                            <img
+                                id="userHeader"
+                                src={user.avatarUrl || "/images/userIcon.png"}
+                                alt="Иконка пользователя"
+                            />
+                        </button>
+                    ) : (
+                        <NavLink to="/login" className={({ isActive }) => (isActive ? 'active' : '')}>
+                            {({ isActive }) => (
+                                <img
+                                    src={isActive ? "/icons/user-active.png" : "/icons/user-line.png"}
+                                    alt="иконка входа"
+                                />
+                            )}
+                        </NavLink>
+                    )}
+                </div>
+                <ModalUser
+                    isOpen={isModalOpen}
+                    onClose={() => setIsModalOpen(false)}
+                    onLogout={handleLogout}
+                />
             </footer>
         </>
     );
-};
+}
 
 export default Footer;
